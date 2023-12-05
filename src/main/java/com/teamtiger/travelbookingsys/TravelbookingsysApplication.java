78978bdb51fa0b46c9889215a9abe0174853d221
Samuel Parvu <samuel.parvu.work@gmail.com>
2023-12-06 00:19:03 +0200

    first propper commit

---
+++ src/main/java/com/teamtiger/travelbookingsys/TravelbookingsysApplication.java
@@ -0,0 +1,13 @@
+package com.teamtiger.travelbookingsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelbookingsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelbookingsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamtiger/travelbookingsys/TravelbookingsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamtiger.travelbookingsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelbookingsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
